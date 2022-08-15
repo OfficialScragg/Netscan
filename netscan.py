5c57eabf52a42ba1902ecb45b8c242e080a6d838
Scragg <danielscragg@outlook.com>
2022-08-16 00:38:38 +0200

    Progress bar

--- netscan.py
+++ netscan.py
@@ -1,37 +1,37 @@
 #!/usr/bin/env python3
 
-import threading
-import socket
+import threading, socket, progressbar
 
 # Settings
 base_ip = '139.162.248.'
-ip_range = range(130, 150)
-ports = range(19, 30)
+ip_range = range(1, 255)
+ports = [20, 21, 22, 23, 25, 53, 80,110, 119, 123, 143, 161, 194, 443] # common ports
 out = open('scan-res.txt', 'w')
 
 # Variables
 open_threads = 0
+widgets = [ 'Scanning...', progressbar.Bar('#') ]
+bar = progressbar.ProgressBar(widgets=widgets).start()
 
 def scan(ip, ports):
-    global open_threads, out
+    global open_threads, out, bar
     res = ip+" -> "
-    print ('Scanning: ', ip)
     for p in ports:
         with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
             s.settimeout(5)
             conn = s.connect_ex((ip, p))
             if(conn == 0) :
-                print(str(ip)+':%d -> OPEN' % (p,))
                 if res == ip+" -> ":
                     res = res + str(p)
                 else:
                     res = res + "," + str(p)
             s.close()
-    print(ip, "scanned!")
     out.write(res+"\n")
     open_threads-=1
+    bar.update(((len(ip_range)-open_threads)/len(ip_range))*100)
     if(open_threads == 0):
-        print("---- Done ----")
+        bar.update(100)
+        print()
 
 def setup():
     global open_threads
